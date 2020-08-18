6f623e1a689ebbbd26870d913a6bc6e11284fcb8
DaniTakacs <takacs.d24@gmail.com>
2020-08-18 13:05:01 +0200

    5th exercise error

---
+++ DemoDynamic/DemoDynamic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AssemblyName theName = new AssemblyName();
+        theName.Name = "DemoAssembly";
+        theName.Version = new Version("1.0.0.0");
+
+        AppDomain domain = AppDomain.CurrentDomain;
+
+        AssemblyBuilder assemBldr = AppDomain.
+    }
+}
