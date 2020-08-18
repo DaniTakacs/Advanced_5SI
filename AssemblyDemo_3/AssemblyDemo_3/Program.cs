042b042e8445631f1e8ba23ad860621ad2235596
DaniTakacs <takacs.d24@gmail.com>
2020-08-18 12:45:25 +0200

    3rd exercise finished

---
+++ AssemblyDemo_3/AssemblyDemo_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyDemo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.ServiceProcess.dll";
+            BindingFlags flags = BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance;
+
+            Assembly assembly = Assembly.LoadFrom(path);
+            Console.WriteLine(assembly.FullName);
+            Type[] types = assembly.GetTypes();
+
+            foreach (var type in types)
+            {
+                Console.WriteLine(" Type: {0}", type.Name);
+                MemberInfo[] members = type.GetMembers(flags);
+
+                foreach (MemberInfo member in members)
+                {
+                    Console.WriteLine(" {0}: {1}", member.MemberType, member.Name);
+                }
+            }
+        }
+    }
+}
